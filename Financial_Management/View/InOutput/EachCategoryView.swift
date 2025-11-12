84c1635898f6823d47012ceb5192f09ea408345f
Elqmidl625 <quoctrung6225@gmail.com>
2025-11-12 21:39:20 +0700

    Update InOutputView UI

--- Financial_Management/View/InOutput/EachCategoryView.swift
+++ Financial_Management/View/InOutput/EachCategoryView.swift
@@ -15,20 +15,24 @@ struct EachCategoryView: View {
     
     
     var body: some View {
-            if Selected == categories.id.hashValue {
-                ButtonCustom(imageName: categories.imageName,
-                             name: categories.name, 
-                             textColor: true)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 15)
-                        .stroke(Color.blue, lineWidth: 2)
-                )
-                
-            } else {
-                ButtonCustom(imageName: categories.imageName,
-                             name: categories.name,
-                             textColor: false)
-            }
+        if Selected == categories.id.hashValue {
+            ButtonCustom(imageName: categories.imageName,
+                         name: categories.name,
+                         textColor: true)
+            .overlay(
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.blue, lineWidth: 2)
+            )
+            
+        } else {
+            ButtonCustom(imageName: categories.imageName,
+                         name: categories.name,
+                         textColor: false)
+            .overlay(
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
+        }
     }
 }
 
@@ -40,26 +44,17 @@ struct ButtonCustom: View {
     @State var textColor: Bool
     
     var body: some View {
-        ZStack {
-            Rectangle()
-                .frame(width: 100,
-                       height: 100)
-                .foregroundColor(.gray)
-                .opacity(0.5)
-                .cornerRadius(15)
+        VStack (spacing: 10) {
+            Image(imageName)
+                .resizable()
+                .frame(width: 30, height: 30)
             
-            VStack (spacing: 5) {
-                Image(imageName)
-                    .resizable()
-                    .frame(width: 60,
-                           height: 60)
-                
-                Text(name)
-                    .font(.system(size: 15))
-                    .fontWeight(.bold)
-                    .foregroundColor(textColor ? .blue : .gray)
-            }
+            Text(name)
+                .font(.system(size: 13))
+                .fontWeight(.bold)
+                .foregroundColor(textColor ? .blue : .gray)
         }
+        .frame(width: 100, height: 75)
     }
 }
 
